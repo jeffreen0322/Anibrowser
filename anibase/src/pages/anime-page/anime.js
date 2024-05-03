47d6ed297e23b41feb73aedf49b0db9862b80c6d
jeffreen0322 <jeffreyenguyen@gmail.com>
2024-05-03 14:14:12 -0700

    Creation of indiv anime page
    + js
    + css

---
+++ anibase/src/pages/anime-page/anime.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import AnimeEntry from "../../components/anime-entry/animeEntry";
+import Category from "../../components/named-header/category";
+import "./anime.css";
+
+export default function SeasonalList() {
+  const idObj = useParams("id");
+  const [anime, SetAnime] = useState([]);
+
+  // Async function to fetch from api.
+  const GetAnime = async () => {
+    const temp = await fetch("https://api.jikan.moe/v4/anime/" + idObj.id).then(
+      (res) => res.json()
+    );
+
+    // Set the top animes.
+    SetAnime(temp.data);
+  };
+
+  // Used when fetching data.
+  useEffect(() => {
+    GetAnime();
+  });
+
+  console.log(anime);
+
+  return (
+    <div>
+      <Category name={anime.title} />
+      <div className="seasonal">
+        <AnimeEntry
+          key={anime.title}
+          id={anime.mal_id}
+          title={anime.title}
+          score={anime.score}
+          episodes={anime.episodes}
+        />
+      </div>
+    </div>
+  );
+}
