af060db32656bdd06676094455f75c3872366674
jeffreen0322 <jeffreyenguyen@gmail.com>
2024-05-03 20:36:44 -0700

    Update anime.js

--- anibase/src/pages/anime-page/anime.js
+++ anibase/src/pages/anime-page/anime.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import AnimeEntry from "../../components/anime-entry/animeEntry";
 import Category from "../../components/named-header/category";
-import "./anime.css";
 
 export default function AnimePage() {
   const idObj = useParams("id");
-  const [anime, SetAnime] = useState([]);
+  const [anime, SetAnime] = useState({
+    mal_id: "",
+    title: "",
+    score: "",
+    episodes: "",
+    images: { jpg: "" },
+  });
 
   // Async function to fetch from api.
   const GetAnime = async () => {
@@ -23,20 +27,42 @@ export default function AnimePage() {
     GetAnime();
   });
 
-  console.log(anime);
+  console.log(anime.images.jpg.image_url);
 
   return (
-    <div>
+    <div className="container">
       <Category name={anime.title} />
-      <div className="seasonal">
-        <AnimeEntry
-          key={anime.title}
-          id={anime.mal_id}
-          title={anime.title}
-          score={anime.score}
-          episodes={anime.episodes}
-        />
-      </div>
+      <AnimeInfo data={anime} />
     </div>
   );
 }
+
+// TODO: Make this exportable (It's own component).
+function AnimeInfo({ data }) {
+  return (
+    <div className="ani=info">
+      <img src={data.images.jpg.image_url} alt={data.title} />
+      <AnimeTextInfo data={data} />
+    </div>
+  );
+
+  function AnimeTextInfo({ data }) {
+    return (
+      <div>
+        <h3>Score: {data.score}</h3>
+        {data.episodes == null ? (
+          <h3>Episodes: N/A</h3>
+        ) : (
+          <h3>Episodes: {data.episodes}</h3>
+        )}
+      </div>
+    );
+  }
+}
+
+// key={anime.title}
+// id={anime.mal_id}
+// title={anime.title}
+// score={anime.score}
+// episodes={anime.episodes}
+// image={anime.images.jpg.image_url}
