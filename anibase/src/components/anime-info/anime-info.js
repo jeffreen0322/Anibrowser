596e8e6d41bad10316448396790e9876d0dfe356
jeffreen0322 <jeffreyenguyen@gmail.com>
2024-05-10 11:54:09 -0700

    Update anime-info.js

--- anibase/src/components/anime-info/anime-info.js
+++ anibase/src/components/anime-info/anime-info.js
@@ -6,7 +6,11 @@ import "./anime-info.css";
 export default function AnimeInfo({ data }) {
   return (
     <div className="ani-info">
-      <Trailer trailer={data.trailer.embed_url} />
+      {data.trailer.embed_url != null ? (
+        <Trailer trailer={data.trailer.embed_url} />
+      ) : (
+        <img src={data.images.jpg.image_url} alt={data.title} />
+      )}
       <AnimeTextInfo data={data} />
       <Synopsis plot={data.synopsis} />
     </div>
