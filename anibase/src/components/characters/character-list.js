204e93c86ec58617d2e0fab3889749a3c2e8b721
jeffreen0322 <jeffreyenguyen@gmail.com>
2024-06-28 11:42:01 -0700

    Re-added characters

--- anibase/src/components/characters/character-list.js
+++ anibase/src/components/characters/character-list.js
@@ -5,32 +5,33 @@ import "./character-list.css";
 export default function CharacterList({ id }) {
   const [characters, SetCharacters] = useState();
 
-  // // Async function to fetch from api.
-  // const GetCharacters = async () => {
-  //   const temp = await fetch(
-  //     "https://api.jikan.moe/v4/anime/" + id + "/characters"
-  //   ).then((res) => res.json());
+  // Async function to fetch from api.
+  const GetCharacters = async () => {
+    const temp = await fetch(
+      "https://api.jikan.moe/v4/anime/" + id + "/characters"
+    ).then((res) => res.json());
 
-  //   // Set the top animes.
-  //   SetCharacters(temp.data);
-  // };
+    // Set the top animes.
+    SetCharacters(temp.data);
+  };
 
-  // // // Used when fetching data.
-  // useEffect(() => {
-  //   GetCharacters();
-  // });
+  // // Used when fetching data.
+  useEffect(() => {
+    GetCharacters();
+  });
+
+  console.log(characters);
 
   return (
-    // <div className="characters">
-    //   {characters.map((data) => (
-    //     <Character
-    //       key={data.character.mal_id}
-    //       role={data.role}
-    //       name={data.character.name}
-    //       image={data.character.images.jpg.image_url}
-    //     />
-    //   ))}
-    // </div>
-    null
+    <div className="characters">
+      {characters.map((data) => (
+        <Character
+          key={data.character.mal_id}
+          role={data.role}
+          name={data.character.name}
+          image={data.character.images.jpg.image_url}
+        />
+      ))}
+    </div>
   );
 }
