395bab6766106d98dcb95ebf8d2b686855ff8223
jeffreen0322 <jeffreyenguyen@gmail.com>
2024-09-25 12:07:21 -0700

    Update category.js

--- anibase/src/components/named-header/category.js
+++ anibase/src/components/named-header/category.js
@@ -3,7 +3,7 @@ import "./category.css";
 
 export default function Category({ name }) {
   return (
-    <div className="category-container" id="category-header">
+    <div className="category-container">
       <h2 className="category">{name}</h2>
     </div>
   );
