ca13ee09c21e6552aab765746629415e55572e63
jeffreen0322 <jeffreyenguyen@gmail.com>
2024-09-25 14:39:46 -0700

    Added Chevron on page

--- anibase/src/pages/reccomendation/reccomendation.js
+++ anibase/src/pages/reccomendation/reccomendation.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import RecommendedAnime from "../../components/recommended/recommended";
+import UpChevron from "../../components/redirect/up/up";
 import "./reccomendation.css";
 
 export default function ReccomendationPage() {
   const idObj = useParams("id");
-  return <RecommendedAnime id={idObj.id} limit={{ entries: -1 }} />;
+  return (
+    <div>
+      <RecommendedAnime id={idObj.id} limit={{ entries: -1 }} />;
+      <UpChevron id="navigation" />
+    </div>
+  );
 }
