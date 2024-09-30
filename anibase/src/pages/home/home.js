494447afad4527d926c67ec165cede9e704b16ea
jeffreen0322 <jeffreyenguyen@gmail.com>
2024-09-30 12:24:21 -0700

    Adjustments to Home

--- anibase/src/pages/home/home.js
+++ anibase/src/pages/home/home.js
@@ -2,24 +2,16 @@ import React from "react";
 import "./home.css";
 import SeasonalList from "../../components/seasonalList";
 import UpChevron from "../../components/redirect/up/up";
+import AnibrowserGraphic from "../../components/graphics/anibrowser-graphic";
 import Footer from "../../components/footer/footer";
-import myImage from "./images/anibrowser.png";
 
 export default function HomePage() {
   return (
     <div className="homePage">
-      <MainPic />
+      <AnibrowserGraphic />
       <SeasonalList />
       <UpChevron id="navigation" />
       <Footer />
-    </div>
-  );
-}
-
-function MainPic() {
-  return (
-    <div className="picture">
-      <img src={myImage} alt="Anibrowser Picture" id="anibrowser" />
     </div>
   );
 }
