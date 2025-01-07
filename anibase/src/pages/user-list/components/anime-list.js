0dc48ccaf7792221a1013fd97dcae42928446e62
jeffreen0322 <jeffreyenguyen@gmail.com>
2025-01-06 19:20:01 -0800

    Adjusted responsiveness for user-anime-entry

--- anibase/src/pages/user-list/components/anime-list.js
+++ anibase/src/pages/user-list/components/anime-list.js
@@ -43,11 +43,15 @@ export const AnimeTable = ({ userId }) => {
           <li key={key}>
             <Link className="anime-container" to={`/anime/${anime.ani_id}`}>
               <img src={anime.image_url} alt={anime.anime} />
-              <p id="title">{anime.name}</p>
-              <p id="status">
+              <p className="descriptor" id="title">
+                {anime.name}
+              </p>
+              <p className="descriptor" id="status">
                 {anime.status.slice(0, 1).toUpperCase() + anime.status.slice(1)}
               </p>
-              <p id="rating">{!anime.rating ? "N/A" : anime.rating}</p>
+              <p className="descriptor" id="rating">
+                {!anime.rating ? "N/A" : anime.rating}
+              </p>
             </Link>
           </li> // Use a unique key for each list item
         ))
